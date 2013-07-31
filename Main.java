767261f6d8200636ea2aa34a4389fecf0103dfcd
araishikeiwai <rick@araishikeiwai.com>
2013-07-31 09:53:59 +0700

    Main updated

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] araishikeiwai) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Choose what folder to index!");
+        String input = reader.readLine();
+        System.out.println("Choose where to put the index file!");
+        String output = reader.readLine();
+
+        System.out.println("Starting indexing folder...");
+        new DanielIndexer(input, output).executeIndex();
+    }
+}
